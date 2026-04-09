c784444601bb46311527ae65d782c72b5bb56c10
Jorge Luis Calleja Alvarado <jocalleja@corpcab.com.mx>
2026-04-09 12:00:51 -0600

    config set

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">Hola mundo</div>
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">Hola mundo v1</div>
       </div>
     </div>
   );
